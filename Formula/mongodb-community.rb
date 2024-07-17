fc82fc81730b863ebf51ff429b8e7f828147b3ce
yjstyle <yjstyle@users.noreply.github.com>
2024-07-17 14:48:22 +0900

    Update mongodb-community.rb
    For Linux

--- Formula/mongodb-community.rb
+++ Formula/mongodb-community.rb
@@ -5,11 +5,11 @@ class MongodbCommunity < Formula
   # frozen_string_literal: true
 
   if Hardware::CPU.intel?
-    url "https://fastdl.mongodb.org/osx/mongodb-macos-x86_64-7.0.12.tgz"
-    sha256 "b0a7e0d44a5143b2f6ae0240ad11d02eb6b053c6e1e369628a1e46476ff78e89"
+    url "https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu2204-7.0.12.tgz"
+    #sha256 "b0a7e0d44a5143b2f6ae0240ad11d02eb6b053c6e1e369628a1e46476ff78e89"
   else
-    url "https://fastdl.mongodb.org/osx/mongodb-macos-arm64-7.0.12.tgz"
-    sha256 "5e4152b8a2b1812468cb3ceb3d86a613fe38155f28975db99ea0ce07d12748c3"
+    url "https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu2204-7.0.12.tgz"
+    #sha256 "5e4152b8a2b1812468cb3ceb3d86a613fe38155f28975db99ea0ce07d12748c3"
   end
 
   option "with-enable-test-commands", "Configures MongoDB to allow test commands such as failpoints"
@@ -29,7 +29,7 @@ class MongodbCommunity < Formula
     end
 
     prefix.install Dir["*"]
-    prefix.install_symlink "macos_mongodb.plist" => "#{plist_name}.plist"
+    #prefix.install_symlink "macos_mongodb.plist" => "#{plist_name}.plist"
   end
 
   def post_install
